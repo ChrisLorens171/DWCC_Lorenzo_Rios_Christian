4f5b1a0fd9ae2514d39f4e6a01e34b14e155c553
ChrisLorens171 <160019552+ChrisLorens171@users.noreply.github.com>
2024-10-21 12:36:57 +0200

    Delete DOM directory

--- DOM/Ejemplo/script/my_script.js
+++
@@ -1,9 +0,0 @@
-const $d = document
-
-const $main=$d.querySelector("main")
-
-const $p=$d.createElement("p")
-const $node=$d.createTextNode("Parrafo creado desde javascript")
-$p.appendChild($node)
-
-$main.appendChild($p)
