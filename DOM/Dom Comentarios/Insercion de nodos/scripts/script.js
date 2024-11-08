6bfa2cedb390c1d1b463d1603f02c2012f8933d0
ChrisLorens171 <chrislorens019@gmail.com>
2024-11-08 14:24:53 +0100

    dom

---
+++ DOM/Dom Comentarios/Insercion de nodos/scripts/script.js
@@ -0,0 +1,136 @@
+const $d=document,
+        // $forms=$d.forms[0]
+        $form=$d.querySelector("form"),
+        $textArea=$d.querySelector("#textArea"),
+        $comments=$d.querySelector("#comentarios"),
+        $template=$d.querySelector("#template-comentario").content,
+        //$deleteComment=$d.querySelector("#deleteNode"),
+        $commentCount=$d.querySelector("#commentCount"),
+        [$addComment,$deleteComment,$insertComment]=$d.getElementsByName("nodeAction")
+
+const comments=[]
+
+    
+/* let nComentarios = 1 */
+
+/* $form.addEventListener("submit", ev=> {
+    ev.preventDefault()
+
+    if($textArea.value.length) {
+
+        const $article=$d.createElement("article")
+    
+        const $h4=$d.createElement("h4")
+        const $cabecera=$d.createTextNode(`Comentario ${nComentarios}`)
+        $h4.appendChild($cabecera)
+    
+        const $p=$d.createElement("p")
+        const $texto=$d.createTextNode($textArea.value)
+        $p.appendChild($texto)
+
+        $article.append($h4, $p)
+
+        $comments.appendChild($article)
+
+        nComentarios++
+        $form.reset()
+        $textArea.focus()
+
+    } else {
+        alert("Escribe algo crack!")
+    }
+}) */
+
+/* $form.addEventListener("submit", ev=> {
+    ev.preventDefault()
+
+    const $comentario=$template.cloneNode(true)
+
+    $comentario.querySelector("h4").innerHtml=`Comentario ${nComentarios}`
+    $comentario.querySelector("p").textContent=$textArea.value
+
+    $comments.appendChild($comentario)
+    $form.reset()
+    $textArea.focus()
+    nComentarios++
+}) */
+
+/* $form.addEventListener("submit", ev=> {
+    ev.preventDefault()
+
+    $comments.innerHTML+=`
+        <article>
+            <h4>Comentario ${nComentarios}</h4>
+            <p>${$textArea.value}</p>
+        </article>`
+
+    $form.reset()
+    $textArea.focus()
+    nComentarios++
+}) */
+
+/* function renderComments(comments) {
+
+    $comments.innerHTML = "";
+
+    const $fragmento=$d.createDocumentFragment()
+
+    comments.forEach((comment, i) => {
+        const $comentario = $template.cloneNode(true);
+        $comentario.querySelector("h4").innerHTML = `Comentario ${i + 1}`; // Corrected innerHTML.
+        $comentario.querySelector("p").textContent = comment.texto;
+        $fragmento.appendChild($comentario);
+    });
+    $comments.appendChild($fragmento);
+    
+    $form.reset();
+    $textArea.focus();
+} */
+    
+function renderComments(comments) {
+    /* $comments.innerHTML=comments.map((comment, i)=>`
+        <article>
+        <h4>Comentario ${i+1}</h4>
+        <p>${comment.texto}</p>
+        </article>`).join(``) */
+
+    $comments.innerHTML=comments.reduce((anterior, actual, i) => anterior+`
+        <article>
+        <h4>Comentario ${i+1}</h4>
+        <p>${actual.texto}</p>
+        </article>`,'')
+
+    $commentCount.innerHTML=comments.map((comments, i) => 
+        `<option value="${i}"> 
+            ${i+1}
+        </option)>`).join('')
+
+    $form.reset()
+    $textArea.focus()
+}
+
+$form.addEventListener("submit", ev => {
+    ev.preventDefault()
+
+    if ($addComment.checked) {
+        const comment = {
+            id: new Date().getTime(),
+            texto: $textArea.value
+        }
+        
+        comments.push(comment)
+    }
+
+    if ($deleteComment.checked) {
+        comments.splice($commentCount.value, 1)
+    }
+
+    renderComments(comments)
+});
+
+/* $deleteComment.addEventListener("click", ev => {
+    ev.preventDefault()
+
+    comments.pop()
+    renderComments(comments)
+}) */
