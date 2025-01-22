56891e50044637b38dbb913641c007f2cc268757
ChrisLorens171 <chrislorens019@gmail.com>
2025-01-22 11:44:22 +0100

    Ajax

--- Ajax/Ejemplos/scripts/script.js
+++ Ajax/Ejemplos/scripts/script.js
@@ -1,5 +1,5 @@
 const $d=document,
-     $tbody=$d.querySelector("tbody"),
+     $carrito=$d.querySelector("tbody"),
      $cards=$d.querySelector(".cards"),
      $totalPrice=$d.querySelector(".submenu").querySelector("span"),
      $vaciarBtn=$d.querySelector("#vaciar-carrito")
@@ -43,15 +43,46 @@ function renderCursos(cursos) {
     `,'')
 }
 
-function renderCarrito(carritos) {
-    //console.log(carritos)
+function renderCarritos(carritos) {
+    console.log(carritos)
     let totalPrice=carritos.reduce((anterior,actual)=>{
         let newPrice=cursos.find(curso=>curso.id==actual.cursoId).newPrice
-        console.log(newPrice,actual.cantidad)
         return anterior+newPrice*actual.cantidad
     },0)
-    $totalPrice.innerHTML=`${totalPrice} &euro;` 
+    $totalPrice.innerHTML=`${totalPrice} &euro;`
+
+    $carrito.innerHTML=carritos.map(el=>`
+        <tr>
+            <td><img src="${el.curso.src}" class="imagen-curso u-full-width" /></td>
+            <td>${el.curso.title}</td>
+            <td>${el.curso.newPrice}</td>
+            <td>${el.cantidad}</td>
+            <td><i class="fa fa-trash" data-id="${el.id}"></i></td>
+        </tr>`).join('')
+
+    carritos.length 
+        ?$vaciarBtn.style.display="block"
+        :$vaciarBtn.style.display="none"
+    
 }
+
+$vaciarBtn.addEventListener("click",ev=>{
+    ev.preventDefault()
+    //let idBorrar=Array.from($carrito.querySelectorAll("i")).map(el=>el.dataset.id)
+    Promise.all(
+    Array.from($carrito.querySelectorAll("i"))
+    .map(el=>fetch(`http://localhost:3000/carritos/${el.dataset.id}`,{
+        method:"DELETE"
+    })))
+    .then(respuestas=>respuestas
+        .every(resp=>resp.ok)
+        ?respuestas.map(resp=>resp.json())
+        :respuestas.map(resp=>Promise.reject(resp)))
+
+    .then(jsons=>getCursos())
+    .catch(errors=>console.log(errors))
+    
+})
 
 function getCursos(){
     ajax({
@@ -60,8 +91,8 @@ function getCursos(){
             cursos.splice(0,cursos.length,...json)
             renderCursos(cursos)
             ajax({
-                url:"http://localhost:3000/carritos",
-                fExito:(json)=>renderCarrito(json),
+                url:"http://localhost:3000/carritos?_embed=curso",
+                fExito:(json)=>renderCarritos(json),
                 fError:error=>console.log(error)
             })
         },
@@ -70,13 +101,71 @@ function getCursos(){
     })
 }
 
+$carrito.addEventListener("click",ev=>{
+    let id=ev.target.dataset.id
+    console.log(id)
+    if (id) {
+        ajax({
+            url:`http://localhost:3000/carritos/${id}`,
+            method:"DELETE",
+            fExito:(json)=>{
+                getCursos()
+            },
+            fError:(error)=>console.log(error)
+        })
+    }
+})
+
+function addCarritoCurso(cursoId) {
+    ajax({
+        url:"http://localhost:3000/carritos",
+        method: "POST",
+        fExito:json=>{
+            getCursos()
+        },
+        fError:error=>console.log(error),
+        data:{
+            cursoId,
+            cantidad:"1",
+            usuarioId:"1"
+        }
+    })
+}
+
+function updateCarritoCurso(cursoCarrito) {
+    //console.log(cursoCarrito)
+    ajax({
+        url:`http://localhost:3000/carritos/${cursoCarrito.id}`,
+        method:"PATCH",
+        fExito:json=>{
+            getCursos()
+        },
+        fError:error=>console.log(error),
+        data:{
+            cantidad:parseInt(cursoCarrito.cantidad)+parseInt(1),
+        }
+    })
+}
+
 $cards.addEventListener("click",ev=>{
     ev.preventDefault()
 
     let cursoId=ev.target.dataset.cursoId
     if(cursoId) {
-        console.log(cursoId)
-
+        //console.log(cursoId)
+        ajax({
+            url:`http://localhost:3000/carritos?cursoId=${cursoId}`,
+            fExito:(json)=>{
+                if(json.length) {
+                    // Actualizar cantidad
+                    updateCarritoCurso(json[0])
+                } else {
+                    // Añadir curso
+                    addCarritoCurso(cursoId)
+                }
+            },
+            fError:error=>console.log(error)
+        })
     }
 })
 
