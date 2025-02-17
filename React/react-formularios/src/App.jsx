6811838325563dd440d3e395763f2b2e3add5cf4
ChrisLorens171 <chrislorens019@gmail.com>
2025-02-17 15:17:38 +0100

    react

--- React/react-formularios/src/App.jsx
+++ React/react-formularios/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import Form1 from './components/Form1'
+import Form2 from './components/Form2'
 
 function App() {
-  return <Form1/>
+  return <Form2/>
 }
 
 export default App
--- React/react-formularios/src/components/Form1.jsx
+++ React/react-formularios/src/components/Form1.jsx
@@ -2,35 +2,67 @@ import React from 'react'
 import { useState } from 'react'
 
 function Form1() {
-    const [nombre,setNombre]=useState("")
-    const [sexo,setSexo]=useState("hombre")
-    
-  return (
-    <form>
-        <label htmlFor="nombre">Nombre</label>
-        <input type="text" 
-               id='nombre' 
-               name='nombre' 
-               value={nombre} 
-               onChange={(ev)=>setNombre(ev.target.value)}/>
+       const [nombre,setNombre]=useState("")
+       const [sexo,setSexo]=useState("hombre")
+       const [lenguaje,setLenguaje]=useState("js")
+       const [terminos,setTerminos]=useState(true)
 
-    <p>Sexo:</p>
-    <input type="radio" 
-           name="sexo" 
-           id="hombre" 
-           value="hombre" 
-           onChange={ev=>setSexo(ev.target.value)}/>
-    <label htmlFor="hombre">Hombre</label>
-    
-    <input type="radio" 
-           name="sexo" 
-           id="mujer" 
-           value="mujer" 
-           onChange={ev=>setSexo(ev.target.value)}/>
-    <label htmlFor="mujer">Mujer</label>
+       const handleSubmit=ev=>{
+              ev.preventdefault()
+              alert("El formulario se ha enviado")
+       }
 
-    </form>
-  )
+       return (
+              <form onSubmit={handleSubmit}>
+                     <label htmlFor="nombre">Nombre</label>
+                     <input type="text" 
+                            id='nombre' 
+                            name='nombre' 
+                            value={nombre} 
+                            onChange={(ev)=>setNombre(ev.target.value)}/>
+
+                     <p>Sexo:</p>
+                     <input type="radio" 
+                            name="sexo" 
+                            id="hombre" 
+                            value="hombre" 
+                            defaultChecked
+                            onChange={ev=>setSexo(ev.target.value)}/>
+                     <label htmlFor="hombre">Hombre</label>
+
+                     <input type="radio" 
+                            name="sexo" 
+                            id="mujer" 
+                            value="mujer" 
+                            onChange={ev=>setSexo(e.target.value)}/>
+                     <label htmlFor="mujer">Mujer</label>
+
+                     <p>Lengujae de programación favorito</p>
+                     <select 
+                            name="lenguaje" 
+                            value={lenguaje}
+                            onChange={e=>setLenguaje(e.target.value)}
+                     >
+                            <option value="---">---</option>
+                            <option value="js">Javascript</option>
+                            <option value="php">PHP</option>
+                            <option value="py">Python</option>
+                     </select>
+
+                     <hr />
+                     <label htmlFor="terminos">Acepto los terminos y condiciones</label>
+                     <input type="checkbox" 
+                            name="terminos" 
+                            id="terminos" 
+                            onChange={e=>setTerminos(e.target.checked)}
+                            checked={terminos}
+                            />
+
+                     <hr />
+                     <input type="submit" value="Enviar" />
+              </form>
+
+       )
 }
 
 export default Form1
